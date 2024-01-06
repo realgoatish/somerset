a3517a4715e24088fde5799c55b9e809e7537a27
Mike Lamb <mflambjr@gmail.com>
2024-01-05 23:41:25 -0500

    upgrade to Svelte 4 & SK 2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,30 @@
 module.exports = {
 	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	rules: {
-		'import/no-extraneous-dependencies': 0,
-		'import/prefer-default-export': 0,
-		'import/no-mutable-exports': 0,
-		'import/extensions': 0,
-		'import/no-unresolved': 0,
-		'import/first': 0,
-		'no-use-before-define': 0,
-		'no-undef-init': 0,
-		'no-underscore-dangle': 0,
-		'import/newline-after-import': 0,
-		'no-shadow': 0,
-		'@typescript-eslint/no-explicit-any': 0,
-		'default-case': 0,
-		'no-nested-ternary': 0,
-		'consistent-return': 0
-	},
+	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	]
 };
